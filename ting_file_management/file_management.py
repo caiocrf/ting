fd1e797372d06be6ab0082cc5153c62b7e9317e0
caio pinho <icandoit2026@gmail.com>
2023-06-16 11:07:14 -0300

    queue and file_management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,19 @@
+import sys
+import os
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    file_extension = os.path.splitext(path_file)[1]
+
+    if not os.path.exists(path_file):
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+        return []
+
+    if file_extension != ".txt":
+        sys.stderr.write("Formato inválido\n")
+        return []
+
+    with open(path_file, "r") as file:
+        lines = file.read().split("\n")
+
+    return lines
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,20 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqu"""
+        self.data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        return self.data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if len(self.data) == 0:
+            raise IndexError("Não há elementos")
+        return self.data.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if 0 <= index < len(self.data):
+            return self.data[index]
+        raise IndexError("Índice Inválido ou Inexistente")
