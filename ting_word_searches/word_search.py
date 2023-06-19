623ac3ee5634fe84e0f3947ea1aea7729bb7053b
caio pinho <icandoit2026@gmail.com>
2023-06-19 11:10:14 -0300

    ..

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,18 +1,19 @@
 def exists_word(word, instance):
-    search_result = list()
+    search_result = []
+
     for i in range(len(instance)):
         file = instance.search(i)
         occurrence = []
 
         for j, line in enumerate(file["linhas_do_arquivo"], 1):
             if word.lower() in line.lower():
-             occurrence.append({"linha": j})
+                occurrence.append({"linha": j})
 
         if occurrence:
             result = {
-            "palavra": word,
-            "arquivo": file["nome_do_arquivo"],
-            "ocorrencias": occurrence
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": occurrence
             }
             search_result.append(result)
 
